af3e5f30fad1527e71a15f3af8d5717431be2f90
jchan3580 <jchan3580@gmail.com>
2017-03-11 14:32:16 -0500

    prelim models for objects

---
+++ Prototype/core/src/edu/cornell/gdiac/physics/space/PlanetModel.java
@@ -0,0 +1,47 @@
+package edu.cornell.gdiac.physics.space;
+
+import edu.cornell.gdiac.physics.GameCanvas;
+import edu.cornell.gdiac.physics.obstacle.WheelObstacle;
+
+/**
+ * Created by jchan_000 on 3/11/2017.
+ */
+public class PlanetModel extends WheelObstacle{
+
+    // Physics constants
+    /** The density of the character */
+    private static final float PLANET_DENSITY = 1.0f;
+
+    //TODO type of the planet
+    private static int type;
+
+    public void setType(int val){type = val;}
+
+    public int getType(){return type;}
+
+    /**
+     * Creates a new planet at the given position.
+     *
+     * The size is expressed in physics units NOT pixels.  In order for
+     * drawing to work properly, you MUST set the drawScale. The drawScale
+     * converts the physics units to pixels.
+     *
+     * @param x  		Initial x position of the avatar center
+     * @param y  		Initial y position of the avatar center
+     * @param radius		The object radius in physics units
+     * @param t         The type of the planet
+     */
+    public PlanetModel(float x, float y, float radius, int t) {
+        super(x,y,radius);
+        setDensity(PLANET_DENSITY);
+        setFixedRotation(true);
+        type = t;
+
+        setName("Planet");
+    }
+
+    public void drawDebug(GameCanvas canvas) {
+        super.drawDebug(canvas);
+    }
+
+}
