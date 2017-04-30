4ed550cfc4e13a281fef2c667a74d892591ca356
liphilosophie <zingyli@gmail.com>
2017-04-29 21:40:05 -0400

    more fixes

--- Code/core/src/edu/cornell/gdiac/controller/LevelSelect.java
+++ Code/core/src/edu/cornell/gdiac/controller/LevelSelect.java
@@ -341,7 +341,7 @@ public class LevelSelect extends PlayMode {
     public void draw(float dt) {
         super.drawBackground();
         canvas.begin();
-        canvas.draw(levelsTitleTexture, Color.WHITE, text.get(0).x, text.get(0).y/3, text.get(1).x, text.get(1).y*2);
+        canvas.draw(levelsTitleTexture, Color.WHITE, text.get(0).x, text.get(0).y, text.get(1).x, text.get(1).y);
         //canvas.draw(levelsTitleTexture, Color.WHITE, 3*canvas.getWidth() /9 , 2*canvas.getHeight()/8, 3*canvas.getWidth()/9, 2*canvas.getHeight()/8);
         canvas.end();
         super.drawObjects();
